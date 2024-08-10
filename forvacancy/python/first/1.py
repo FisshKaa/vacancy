88708ddb790329b936c4b6d69b1732be19da6627
FisshKaa <72679512+FisshKaa@users.noreply.github.com>
2024-08-10 04:04:44 +0300

    Add files via upload

---
+++ forvacancy/python/first/1.py
@@ -0,0 +1,14 @@
+
+def isEven(value):
+    return value % 2 == 0
+
+print(isEven(4))
+
+
+def difficult_number(value):
+    return not (value & 1)
+
+
+
+print(difficult_number(4))
+
---
+++ forvacancy/python/third/sort.py
@@ -0,0 +1,6 @@
+def sort(list):
+    return sorted(list)
+
+list = [2, 8, 3, 5, 6, 1, 7, 4]
+sorted_list = sort(list)
+print(sorted_list)
